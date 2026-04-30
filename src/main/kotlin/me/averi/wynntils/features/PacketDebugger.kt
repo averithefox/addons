a0bfd2af28844cc502c43ed9cde46aa994869d1c
Averi <git@averi.me>
2026-04-30 11:25:18 +0000

    fix: correct category on extended features

--- src/main/kotlin/me/averi/wynntils/features/PacketDebugger.kt
+++ src/main/kotlin/me/averi/wynntils/features/PacketDebugger.kt
@@ -1,9 +1,12 @@
 package me.averi.wynntils.features
 
+import com.wynntils.core.persisted.config.Category
+import com.wynntils.core.persisted.config.ConfigCategory
 import com.wynntils.features.debug.PacketDebuggerFeature
 import me.averi.wynntils.dx.Configurable
 import me.averi.wynntils.dx.Setting
 
+@ConfigCategory(Category.DEBUG)
 object PacketDebugger : PacketDebuggerFeature(), Configurable {
   override val settings: MutableList<Setting<*>> = ArrayList()
 
--- src/main/kotlin/me/averi/wynntils/features/QuickCast.kt
+++ src/main/kotlin/me/averi/wynntils/features/QuickCast.kt
@@ -1,9 +1,12 @@
 package me.averi.wynntils.features
 
+import com.wynntils.core.persisted.config.Category
+import com.wynntils.core.persisted.config.ConfigCategory
 import com.wynntils.features.combat.QuickCastFeature
 import me.averi.wynntils.dx.Configurable
 import me.averi.wynntils.dx.Setting
 
+@ConfigCategory(Category.COMBAT)
 object QuickCast : QuickCastFeature(), Configurable {
   override val settings: MutableList<Setting<*>> = ArrayList()
 
